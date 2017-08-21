407aec5084b9d6aa80d09506bcbf46bd2c131e50
Jack Stevens <jack@applyr.com>
2017-08-21 12:36:00 -0400

    Fix error when menu toggle is not on page

--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -1,7 +1,9 @@
 export function init () {
   var menuToggle = document.querySelector('.rx-navbar__menu-toggle')
   var menu = document.querySelector('.rx-navbar__menu')
-  menuToggle.onclick = function () {
-    menu.classList.toggle('rx-navbar__menu--mobile-visible')
+  if (menuToggle) {
+    menuToggle.onclick = function () {
+      menu.classList.toggle('rx-navbar__menu--mobile-visible')
+    }
   }
 }
