9a34f07489e04a02289c9654e47a580ba878a2c1
rhys-nicholls <34071702+rhys-nicholls@users.noreply.github.com>
2020-06-11 12:23:05 +0100

    Remove message from  Error page

--- src/react/ErrorPage.js
+++ src/react/ErrorPage.js
@@ -3,17 +3,16 @@ import EmailLink from './EmailLink'
 
 const DEFAULT_ERROR_MSG = 'An unknown error occured'
 
-export default function ErrorPage ({message, eventId}) {
+export default function ErrorPage ({eventId}) {
   const ref = typeof eventId === 'string' ? `${eventId}` : ''
-  const errorMessage = ref ? <span>with the error reference <b>{ref}</b></span> : null
+  const errorMessage = ref ? <span>Error reference <b>{ref}</b></span> : null
   return (
     <div className='rx-error-page rx-container'>
       <div className='rx-grid'>
         <div className='rx-col--md-6 rx-col--md-offset-3'>
           <div className='rx-card'>
             <h1>Oops... Something went wrong</h1>
-            <p>The server came back with the following error:</p>
-            <pre className='rx-error-page__pre'>{message || DEFAULT_ERROR_MSG}</pre>
+            <p>Our development team has been notified.</p>
             <p>For support on this error, please contact <EmailLink subject={`Error Ref: ${ref || 'N/A'}`}>support@riskxchange.co</EmailLink> {errorMessage}</p>
           </div>
         </div>
